4a4f73bb9bb5dc495e3d18ca98f38c8fdb8ae5b9
ryan.baga <ryan.baga@globe.com.ph>
2022-11-07 14:43:53 +0800

    update sample code

--- day2-sample-code/src/routes/book.js
+++ day2-sample-code/src/routes/book.js
@@ -1,4 +1,6 @@
 import express from "express";
+import pick from "lodash/pick.js";
+import { body, validationResult } from "express-validator";
 
 const booksRouter = express.Router();
 
@@ -48,22 +50,60 @@ booksRouter.get("/books/:bookId/author", async (request, response) => {
     });
 })
 
-booksRouter.post("/books", async (request, response) => {
+booksRouter.post(
+    "/books",
+    [
+        body('title')
+        .notEmpty()
+        .isLength({min: 5})
+        .withMessage(
+            "Book requires 'title' and should be minimum of 5 characters!"
+        ),
+    ],
+    async (request, response) => {
+    const error = validationResult(request);
     const body = request.body;
 
+    const filteredBody = pick(request.body, [
+        "title",
+        "subtitle",
+        "published",
+        "publisher",
+        "pages",
+        "description",
+        "website",
+        "authorId"
+    ])
+
     const book = await request.app.locals.prisma.book.create({
-        data: {
-            title: body.title,
-            subtitle: body.subtitle,
-            published: body.published,
-            publisher: body.publisher,
-            pages: body.pages,
-            description: body.description,
-            website: body.website,
-            authorId: body.authorId,
-          },
+        data: filteredBody,
     })
     response.send({data: book, message: 'ok'})
 })
+
+booksRouter.put("/books/:bookId", async (request, response) => {
+    const bookId = request.params.bookId;
+
+    const filteredBody = pick(request.body, [
+        "title",
+        "subtitle",
+        "published",
+        "publisher",
+        "pages",
+        "description",
+        "website",
+        "authorId"
+    ]);
+
+    const updatedBook = await request.app.locals.prisma.book.update({
+        where: {
+            id: Number.parseInt(bookId),
+        },
+        data: filteredBody,
+    });
+
+    response.send({data: updatedBook, message: 'ok'});
+
+});
 
 export default booksRouter;
